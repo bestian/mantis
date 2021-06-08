7b0620c08d585868e3ad5df1c95e3a3207669254
Bestian Tang <bestian@gmail.com>
2021-06-09 07:56:57 +0800

    init

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          螳螂人數學
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>第1版</div>
       </q-toolbar>
     </q-header>
 
@@ -30,13 +30,11 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          項目
         </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item to="/">數字表</q-item>
+        <q-item to="/plus">加法表</q-item>
+        <q-item to="/mul">乘法表</q-item>
       </q-list>
     </q-drawer>
 
@@ -47,60 +45,11 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData
     }
   }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,56 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <table>
+      <tr>
+        <th>
+          人類數學  
+        </th>
+        <th>
+          螳螂人數學  
+        </th>
+      </tr>
+      <tr v-for="i in items" :key="i">
+        <td>
+          {{i }}
+        </td>
+        <td>
+          {{ toM(i) }}
+        </td>
+      </tr>
+    </table>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    }
+  },
+  methods: {
+    toM(num) {
+      var ans = []
+      while (num > 0) {
+        ans.unshift(num %  6)
+        num = Math.floor(num / 6)
+      }
+      return ans.join('')
+    }
+  }
 }
 </script>
+
+<style type="text/css" scoped="">
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  table td, table th {  
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+</style>
